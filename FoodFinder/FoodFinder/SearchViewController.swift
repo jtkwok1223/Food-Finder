24e9241dbd9c166dd2038eb8256d8db1f10062af
Mae Wang <maewang@berkeley.edu>
2019-05-04 16:55:44 -0700

    fixing firebase and search bar

---
+++ FoodFinder/FoodFinder/SearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchViewController.swift
+//  Food Finder
+//
+//  Created by GEGWC10 on 5/4/19.
+//  Copyright © 2019 iOSDecal. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
